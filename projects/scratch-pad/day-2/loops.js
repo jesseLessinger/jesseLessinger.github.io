a645ef4671877a29bb7f0b83bce33a7e8a8c1e84
Jesse Lessinger <jesse.lessinger@gmail.com>
2019-02-04 16:04:38 +0000

    scratch-pad update

--- projects/scratch-pad/day-2/loops.js
+++ projects/scratch-pad/day-2/loops.js
@@ -130,7 +130,7 @@ if((typeof process !== 'undefined') &&
     // here, export any references you need for tests //
     module.exports.printArrayValues = printArrayValues;
     module.exports.printArrayValuesInReverse = printArrayValuesInReverse;
-    module.exports.getObjectValues = getObjectValues;
+    module.exports.getObjectValues = getObjectValues; //Jesse added this line, still doesn't work
     module.exports.printObjectValues = printObjectValues;
     module.exports.getObjectKeys = getObjectKeys;
     module.exports.printObjectKeys = printObjectKeys;
